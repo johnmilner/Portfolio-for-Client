de9448300efb29123dda05a9377d2e44b2d316cd
John Milner <john.milner@mac.com>
2018-03-02 15:37:19 -0800

    add timeline to about section and timeline.scss component

--- js/build.js
+++ js/build.js
@@ -104,6 +104,25 @@ function createProjectGrid() {
       });
     headroom.init();
 
+    //TIMELINE
+    $(function() {
+  
+        var watchScroll =0;
+        var rightComments = $('.r-event .event-body');
+          var leftComments = $('.l-event .event-body');
+          TweenMax.staggerFrom(rightComments, 1, {x: 100, ease:Bounce.easeOut},1);
+          TweenMax.staggerFrom(leftComments, 1, {x: -100,ease:Bounce.easeOut},1);
+        
+          $(window).on('scroll', function() {
+            var scrollTop = $(window).scrollTop();
+            (scrollTop > watchScroll)?
+            $('footer').addClass('footer-up'):
+              $('footer').removeClass('footer-up');
+            
+            watchScroll = scrollTop;
+          })
+        })
+
 
     $('footer').footerReveal({
         shadow: false,
