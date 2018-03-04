16f128ff3e1d643072ee072995052cc3ecc0324f
John Milner <john.milner@mac.com>
2018-03-04 02:18:07 -0800

    add parallax mousemove effect for landing section

--- js/build.js
+++ js/build.js
@@ -77,6 +77,45 @@ function createProjectGrid() {
 }
 */
 
+window.onload = function () {
+
+	var parallaxBox = document.querySelector('.composition');
+	var c1left = document.getElementById ( 'i1' ).offsetLeft,
+	c1top = document.getElementById ( 'i1' ).offsetTop,
+	c2left = document.getElementById ( 'i2' ).offsetLeft,
+	c2top = document.getElementById ( 'i2' ).offsetTop,
+	c3left = document.getElementById ( 'i3' ).offsetLeft,
+	c3top = document.getElementById ( 'i3' ).offsetTop;
+	//c4left = document.getElementById ( 'i4' ).offsetLeft,
+	//c4top = document.getElementById ( 'i4' ).offsetTop;
+	
+	parallaxBox.onmousemove = function ( event ) {
+		event = event || window.event;
+		var x = event.clientX - parallaxBox.offsetLeft,
+		y = event.clientY - parallaxBox.offsetTop;
+		
+		mouseParallax ( 'i1', c1left, c1top, x, y, 5 );
+		mouseParallax ( 'i2', c2left, c2top, x, y, 15 );
+		mouseParallax ( 'i3', c3left, c3top, x, y, 30 );
+		//mouseParallax ( 'l4', c4left, c4top, x, y, 65 );
+	}
+	
+}
+
+function mouseParallax ( id, left, top, mouseX, mouseY, speed ) {
+	var obj = document.getElementById ( id );
+	var parentObj = obj.parentNode,
+	containerWidth = parseInt( parentObj.offsetWidth ),
+	containerHeight = parseInt( parentObj.offsetHeight );
+	obj.style.left = left - ( ( ( mouseX - ( parseInt( obj.offsetWidth ) / 2 + left ) ) / containerWidth ) * speed ) + 'px';
+	obj.style.top = top - ( ( ( mouseY - ( parseInt( obj.offsetHeight ) / 2 + top ) ) / containerHeight ) * speed ) + 'px';
+}
+
+
+
+
+
+
 (function($) {
     "use strict";
 
