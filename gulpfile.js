b111dba7921937937cfb7e77865a3f19a802ea34
John Milner <john.milner@mac.com>
2018-03-05 11:45:13 -0800

    add new gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+/**
+ * Created by Dmytro on 3/27/2016.
+ */
+var browserify = require('browserify'),
+    gulp = require('gulp'),
+    sourcemaps = require('gulp-sourcemaps'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    source = require('vinyl-source-stream'),
+    buffer = require('vinyl-buffer'),
+    browserSync = require('browser-sync');
+
+/* pathConfig*/
+var entryPoint = './js/build.js',
+    browserDir = './',
+    sassWatchPath = './sass/**/*.scss',
+    jsWatchPath = './js/**/*.js',
+    htmlWatchPath = './**/*.html';
+/**/
+
+gulp.task('js', function () {
+    return browserify(entryPoint, {debug: true, extensions: ['es6']})
+        .transform("babelify", {presets: ["es2015"]})
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./build/js'))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('browser-sync', function () {
+    const config = {
+        server: {baseDir: browserDir}
+    };
+
+    return browserSync(config);
+});
+
+gulp.task('sass', function () {
+  return gulp.src(sassWatchPath)
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions']
+    }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./build/css'))
+    .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('watch', function () {
+    gulp.watch(jsWatchPath, ['js']);
+    gulp.watch(sassWatchPath, ['sass']);
+    gulp.watch(htmlWatchPath, function () {
+        return gulp.src('')
+            .pipe(browserSync.reload({stream: true}))
+    });
+});
+
+gulp.task('run', ['js', 'sass', 'watch', 'browser-sync']);
--- js/build.js
+++ js/build.js
@@ -78,113 +78,111 @@ function createProjectGrid() {
 */
 
 
-
-
-
-
-// window.onload = function () {
-
-    
-
-    
-// }
-
-(function($) {
-    "use strict";
-    // cache DOM
-    const navigation = document.querySelector('.container .navigation')
-    const navCheckbox = navigation.querySelector('.navigation__checkbox')
-    const navItems = navigation.querySelectorAll('.navigation__item')
-    navItems.forEach(item =>
-    item.addEventListener('click', _ => navCheckbox.checked = false))
-
-	var parallaxBox = document.querySelector('.composition');
-	var c1left = document.getElementById ( 'i1' ).offsetLeft,
-	c1top = document.getElementById ( 'i1' ).offsetTop,
-	c2left = document.getElementById ( 'i2' ).offsetLeft,
-	c2top = document.getElementById ( 'i2' ).offsetTop,
-	c3left = document.getElementById ( 'i3' ).offsetLeft,
-	c3top = document.getElementById ( 'i3' ).offsetTop;
-	//c4left = document.getElementById ( 'i4' ).offsetLeft,
-	//c4top = document.getElementById ( 'i4' ).offsetTop;
-	
-	parallaxBox.onmousemove = function ( event ) {
-		event = event || window.event;
-		var x = event.clientX - parallaxBox.offsetLeft,
-		y = event.clientY - parallaxBox.offsetTop;
-		
-		mouseParallax ( 'i1', c1left, c1top, x, y, 5 );
-		mouseParallax ( 'i2', c2left, c2top, x, y, 15 );
-		mouseParallax ( 'i3', c3left, c3top, x, y, 30 );
-		//mouseParallax ( 'l4', c4left, c4top, x, y, 65 );
-	}
-    
-    function mouseParallax ( id, left, top, mouseX, mouseY, speed ) {
-        var obj = document.getElementById ( id );
-        var parentObj = obj.parentNode,
-        containerWidth = parseInt( parentObj.offsetWidth ),
-        containerHeight = parseInt( parentObj.offsetHeight );
-        obj.style.left = left - ( ( ( mouseX - ( parseInt( obj.offsetWidth ) / 2 + left ) ) / containerWidth ) * speed ) + 'px';
-        obj.style.top = top - ( ( ( mouseY - ( parseInt( obj.offsetHeight ) / 2 + top ) ) / containerHeight ) * speed ) + 'px';
-    }
-
-    
-    var transEffect = Barba.BaseTransition.extend({
-        start: function(){
-          this.newContainerLoading.then(val => this.fadeInNewcontent($(this.newContainer)));
-        },
-        fadeInNewcontent: function(nc) {
-          nc.hide();
-          var _this = this;
-          $(this.oldContainer).fadeOut(1000).promise().done(() => {
-            nc.css('visibility','visible');
-            nc.fadeIn(1000, function(){
-              _this.done();
-            })
-          });
-        }
+  "use strict";
+  // cache DOM
+  var navigation = document.querySelector(".container.navigation");
+  var navCheckbox = navigation.querySelector(".navigation__checkbox");
+  var navItems = navigation.querySelectorAll(".navigation__item");
+  // navItems.forEach(item =>
+  //   item.addEventListener("click", _ => (navCheckbox.checked = false))
+  // );
+  navItems.forEach(function (item) {
+    return item.addEventListener("click", function (_) {
+      return navCheckbox.checked = false;
     });
-    Barba.Pjax.cacheEnabled = false;
-    Barba.Pjax.getTransition = function() {
-      return transEffect;
+  });
+
+  var parallaxBox = document.querySelector(".containersss.composition");
+  var c1left = document.getElementById("i1").offsetLeft,
+    c1top = document.getElementById("i1").offsetTop,
+    c2left = document.getElementById("i2").offsetLeft,
+    c2top = document.getElementById("i2").offsetTop,
+    c3left = document.getElementById("i3").offsetLeft,
+    c3top = document.getElementById("i3").offsetTop;
+  //c4left = document.getElementById ( 'i4' ).offsetLeft,
+  //c4top = document.getElementById ( 'i4' ).offsetTop;
+
+  parallaxBox.onmousemove = function(event) {
+    event = event || window.event;
+    var x = event.clientX - parallaxBox.offsetLeft,
+      y = event.clientY - parallaxBox.offsetTop;
+
+    mouseParallax("i1", c1left, c1top, x, y, 5);
+    mouseParallax("i2", c2left, c2top, x, y, 15);
+    mouseParallax("i3", c3left, c3top, x, y, 30);
+    //mouseParallax ( 'l4', c4left, c4top, x, y, 65 );
+  };
+
+  function mouseParallax(id, left, top, mouseX, mouseY, speed) {
+    var obj = document.getElementById(id);
+    var parentObj = obj.parentNode,
+      containerWidth = parseInt(parentObj.offsetWidth),
+      containerHeight = parseInt(parentObj.offsetHeight);
+    obj.style.left =
+      left -
+      (mouseX - (parseInt(obj.offsetWidth) / 2 + left)) /
+        containerWidth *
+        speed +
+      "px";
+    obj.style.top =
+      top -
+      (mouseY - (parseInt(obj.offsetHeight) / 2 + top)) /
+        containerHeight *
+        speed +
+      "px";
+  }
+
+  var transEffect = Barba.BaseTransition.extend({
+    start: function() {
+      this.newContainerLoading.then(val =>
+        this.fadeInNewcontent($(this.newContainer))
+      );
+    },
+    fadeInNewcontent: function(nc) {
+      nc.hide();
+      var _this = this;
+      $(this.oldContainer)
+        .fadeOut(1000)
+        .promise()
+        .done(() => {
+          nc.css("visibility", "visible");
+          nc.fadeIn(1000, function() {
+            _this.done();
+          });
+        });
     }
-    Barba.Pjax.start();
+  });
+  Barba.Pjax.cacheEnabled = true;
+  Barba.Dispatcher.on("newPageReady", function() {
+    $(document).ready(function() {
+      // cache DOM
 
+      $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+        if (
+          location.pathname.replace(/^\//, "") ==
+            this.pathname.replace(/^\//, "") &&
+          location.hostname == this.hostname
+        ) {
+          var target = $(this.hash);
+          target = target.length
+            ? target
+            : $("[name=" + this.hash.slice(1) + "]");
+          if (target.length) {
+            $("html, body").animate(
+              {
+                scrollTop: target.offset().top - 60
+              },
+              1000,
+              "easeInOutExpo"
+            );
+            return false;
+          }
+        }
+      });
 
-    // var elem = document.querySelector(".headroom");
-    // var headroom = new Headroom(elem, {
-    //     "offset": 205,
-    //     "tolerance": 5,
-    //     animation: {
-    //         effects: 'rotateY(-25deg)',
-    //         perspectiveDistance: '2000px'
-    //     },
-    //     "classes": {
-    //        initial: "headroom",
-    //        pinned : "headroom--pinned",
-    //        unpinned : "headroom--unpinned"
-    //     }
-    //   });
-    // headroom.init();
-
-
-    
-    // $('#profile-foto').waypoint(function(direction) {
-    //     if (direction === 'down') {
-    //         $('#profile-foto').addClass('fade-in-up');
-    //         $('#profile-foto').removeClass('fadeOutDown');
-    //     } else if (direction === 'up') {
-    //         $('#profile-foto').addClass('fadeOutDown');
-    //         $('#profile-foto').removeClass('fade-in-up');
-    //     }
-    // }, {
-    //     offset: '75%'
-    // });
-    //createProjectGrid();
-
-    mixitup('#mix-wrapper', {
+      mixitup("#mix-wrapper", {
         load: {
-          sort: 'default:asc' /* default:asc */
+          sort: "default:asc" /* default:asc */
         },
         // animation: {
         //     duration: 700,
@@ -193,46 +191,24 @@ function createProjectGrid() {
         //     reverseOut: true
         //   },
         classNames: {
-          block: 'box', /* mixitup */
-          elementFilter: 'filter-btn', /* control */
-          elementSort: 'sort-btn' /* control */
+          block: "box" /* mixitup */,
+          elementFilter: "filter-btn" /* control */,
+          elementSort: "sort-btn" /* control */
         },
         selectors: {
-          target: '.mix-target' /* .mix */
+          target: ".mix-target" /* .mix */
         }
       });
+    });
+  });
 
-
-$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-        
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        if (target.length) {
-            $('html, body').animate({
-                scrollTop: (target.offset().top - 60)
-            }, 1000, "easeInOutExpo");
-            return false;
-        }
-    }
-});
-
-
-$('footer').footerReveal({
-    shadow: false,
-    zIndex: -101
-});
-
-})(jQuery);
-
-
-
-
-
-
-
-
-
-
+  // Barba.Dispatcher.on('newPageReady', () => {
+  //     const routes = new Router(pages);
+  //     routes.loadEvents();
+  //   });
+  Barba.Pjax.getTransition = function() {
+    return transEffect;
+  };
+  Barba.Pjax.start();
 
 
